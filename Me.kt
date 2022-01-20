65f50d4afc2ecccb53fba4d1d3a319246e6b885a
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2022-01-19 23:31:35 -0600

    Add files via upload

---
+++ Me.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Hello my name is Michael Tapia and I am taking MAD 105 to learn how to program with androids and how to make android apps or games")
+    println("I enjoy learning new things, learning how to code more, becoming a better coder, playing video games, making programs that are useful and work and a lot more")
+    println("I also enjoy reading comics or books one of my favorites is a comic named Invincible and although I haven't read it I have watched the show" +
+            "\nIt only has one season as of today but it is a good show to watch and it's not like the regular superhero shows where the superhero will always win" +
+            "\nBut enough of that more about myself is that I enjoy coding and have learned how to program in Python, Html, and CSS all of which are very interesting and fun to use" +
+            "\nI will try my hardest to learn how to code in Kotlin and hopefully create code, apps and even games that will impress you and my classmates.")
+}
