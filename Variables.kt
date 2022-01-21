cc1fe68b1e1c1caca838f96c9c69ca057c794b46
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2022-01-21 14:18:48 -0600

    Add files via upload

---
+++ Variables.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var myByte: Byte = 2
+    var myShort: Short = 20
+    var myInt: Int = 200
+    var myLong: Long = 200000
+    var myFloat: Float = 3.14F
+    var myDouble: Double = 31.415
+    var myBoolean: Boolean = true
+    var myChar = "Hola"
+
+    println("Byte: $myByte")
+    println("Short: $myShort")
+    println("Integer: $myInt")
+    println("Long: $myLong")
+    println("Float: $myFloat")
+    println("Double: $myDouble")
+    println("Boolean: $myBoolean")
+    println("Character: $myChar")
+
+    // Is there any way I could improve my code where I could use less print statements?
+}
