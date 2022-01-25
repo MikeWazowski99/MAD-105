780db784c282ec821acebe6c8b380fa6808e60d9
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2022-01-25 15:47:47 -0600

    Add files via upload

--- If,Else if ,Else.kt
+++ If,Else if ,Else.kt
@@ -1,14 +1,14 @@
 
 fun main(){
-    var temperature = 99.5
+    var temperature = 60
 
 
-    if (temperature < 97.5)
-        println("Temperature is Low")
+    if (temperature > 99.5)
+        println("Temperature is High")
     else if (temperature > 97.5)
         println("Temperature is Normal")
-    else if (temperature >= 99.5)
-        println("Temperature is High")
+    else if (temperature < 97.5)
+        println("Temperature is Low")
 
 
 }
