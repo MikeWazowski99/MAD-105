4f2d9fad2d920e21fba7950cc27f3b6f975f9971
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2022-01-25 14:50:38 -0600

    Add files via upload

---
+++ If,Else if ,Else.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+    var temperature = 99.5
+
+
+    if (temperature < 97.5)
+        println("Temperature is Low")
+    else if (temperature > 97.5)
+        println("Temperature is Normal")
+    else if (temperature >= 99.5)
+        println("Temperature is High")
+
+
+}
